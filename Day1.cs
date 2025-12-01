8b341eeb90289d05711925a9e16be83df53bfc78
Alexander "Ace" Olofsson <ace@haxalot.com>
2025-12-01 12:11:22 +0100

    Add day 1 solution

---
+++ Day1.cs
@@ -0,0 +1,54 @@
+namespace AdventOfCode2025;
+
+public class Day1 : IAoC
+{
+  public int Day => 1;
+
+  List<int> changes = new List<int>();
+
+  public void Input(IEnumerable<string> lines)
+  {
+    foreach (var line in lines)
+    {
+      int mult = line[0] == 'R' ? 1 : -1;
+      changes.Add(int.Parse(line[1..]) * mult);
+    }
+
+    Console.WriteLine($"Read {changes.Count} instructions");
+  }
+
+  public void Part1()
+  {
+    int zero = 0;
+
+    int at = 50;
+    foreach (var change in changes)
+    {
+      at += change;
+      if (at % 100 == 0)
+        zero++;
+    }
+
+    Console.WriteLine($"Hit 0 a total of {zero} times");
+  }
+  public void Part2()
+  {
+    int zero = 0;
+
+    int at = 50 + 100;
+    foreach (var change in changes)
+    {
+      if (at % 100 == 0)
+        zero += Math.Abs(change / 100);
+      else
+      {
+        zero += Math.Abs((at + change) / 100 - at / 100);
+        if (change < 0 && (at + change) % 100 == 0)
+          zero++;
+      }
+      at += change;
+    }
+
+    Console.WriteLine($"Hit 0 a total of {zero} times");
+  }
+}
